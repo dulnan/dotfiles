5bd0d1c4c3f9c67a890dbbd0fbbb8ea7b59a7b16
Jan Hug <dulnan@me.com>
2018-05-11 08:49:11 +0200

    arch config

--- i3/scripts/launchterm.sh
+++ i3/scripts/launchterm.sh
@@ -5,8 +5,8 @@ FOCUSEDOUTPUT=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).outp
 if [ "$FOCUSEDOUTPUT" = "DP-2" ]
 	then
   # High DPI
-  urxvt -fn "xft:Fira Code Retina:style=Regular:size=14:antialias=true" -fb "xft:Fira Code:style=Medium:size=14:antialias=true" -letsp -2
+  urxvt -fn "xft:Fira Code Retina:style=Regular:size=10:antialias=true" -fb "xft:Fira Code:style=Medium:size=10:antialias=true" -letsp -1
 else
   # Low DPI
-  urxvt -fn "xft:Fira Code:style=Regular:size=12:antialias=true" -fb "xft:Fira Code:style=Bold:size=12:antialias=true"
+  urxvt -fn "xft:Fira Code:style=Regular:size=8:antialias=true" -fb "xft:Fira Code:style=Bold:size=8:antialias=true"
 fi
